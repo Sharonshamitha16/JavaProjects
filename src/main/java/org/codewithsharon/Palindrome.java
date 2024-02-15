d0f3a7bfc23b00171a0273abf7cce1d01158e21a
Sharonshamitha16 <91407268+Sharonshamitha16@users.noreply.github.com>
2024-02-15 15:50:47 +0530

    Update Palindrome.java

--- src/main/java/org/codewithsharon/Palindrome.java
+++ src/main/java/org/codewithsharon/Palindrome.java
@@ -6,11 +6,6 @@ public class Palindrome {
     public static void main(String[] args) {
         System.out.print("Google");
         DummyClass.print();
-//        see youtube maven structure and example.
+
     }
-}// try to learn default class or built in class and method.by click control and hover the function.
-// you can travel by using control and hover. control+hover
-// It suggest for betterment of code. So learn by hover.
-// anything you need to create Use this folder only.
-// do once again push and show commit
-/* ok va huh but y there it didn't show anything like this play button this is maven file structure and architecture so it do own duw to dependencies. */
+}
